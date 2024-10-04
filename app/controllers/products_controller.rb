03e239609d836776810da04f2102aa1a5a87c9eb
rsinghbhamra1 <rsinghbhamra@academic.rrc.ca>
2024-10-04 17:12:29 -0500

    Optimized ProductsController#index to prevent N+1 issues

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,7 @@
 class ProductsController < ApplicationController
   def index
-    @products = Product.all
-  end
+    @products = Product.includes(:category).all
+  end  
 
   def show
     @product = Product.find(params[:id])
